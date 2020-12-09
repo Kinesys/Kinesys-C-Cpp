e31d514aaa894f323b7fc8e5931f9957adda8c6c
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-12-10 01:18:51 +0900

    Create Kinesys Char Array.c

---
+++ src/Kinesys Char Array.c
@@ -0,0 +1,11 @@
+//Kinesys Char Array.c
+#include<stdio.h>
+int main() {
+    char str_a[5];
+    str_a[0] = 'H';
+    str_a[1] = 'e';
+    str_a[2] = 'l';
+    str_a[3] = 'l';
+    str_a[4] = 'o';
+    printf(str_a);
+}
