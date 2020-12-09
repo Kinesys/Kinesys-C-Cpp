36f9ae03cfc3424ea52959c01f3cd49554a152f1
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-12-10 02:04:29 +0900

    Create Kinesys Char_array4.c

---
+++ src/Kinesys Char_array4.c
@@ -0,0 +1,11 @@
+//Kinesys Char_array4.c
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char sum_a[3];
+    sum_a[1] = '1';
+    sum_a[2] = '2';
+    sum_a[3] = '3';
+    printf(sum_a);
+}
